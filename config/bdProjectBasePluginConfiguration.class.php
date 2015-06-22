d54c1c6c2c675f3ee010d88ddde2d1b0767c024d
Johannes Tyra <johannes.tyra@braindata.de>
2015-06-22 15:43:27 +0200

    CHANGE: Added Submodule lib/vendor/dompdf // Added /config dir with plugin conf

---
+++ config/bdProjectBasePluginConfiguration.class.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ *
+ */
+class bdProjectBasePluginConfiguration extends sfPluginConfiguration
+{
+  /**
+   *
+   * @return void
+   */
+  public function initialize()
+  {
+    // disable DOMPDF's internal autoloader if you are using Composer
+    define('DOMPDF_ENABLE_AUTOLOAD', false);
+
+    define("DOMPDF_ENABLE_HTML5PARSER", true);
+    define("DOMPDF_ENABLE_FONTSUBSETTING", true);
+    define("DOMPDF_UNICODE_ENABLED", true);
+    define("DOMPDF_ENABLE_CSS_FLOAT", true);
+
+    // include DOMPDF's default configuration
+    require_once sfConfig::get('sf_plugins_dir').DIRECTORY_SEPARATOR.'bdProjectBasePlugin/lib/vendor/dompdf/dompdf_config.inc.php';
+
+  }
+}
