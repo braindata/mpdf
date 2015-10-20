5f69e76eb4708f48ae601a6a0535a134c4302d3e
Johannes Tyra <johannes.tyra@braindata.de>
2015-10-20 10:51:35 +0200

    CHANGE: Check if constants are defined in /bdProjectBasePluginConfiguration.class.php

--- config/bdProjectBasePluginConfiguration.class.php
+++ config/bdProjectBasePluginConfiguration.class.php
@@ -12,13 +12,24 @@ class bdProjectBasePluginConfiguration extends sfPluginConfiguration
   public function initialize()
   {
     // disable DOMPDF's internal autoloader if you are using Composer
-    define('DOMPDF_ENABLE_AUTOLOAD', false);
 
-    define("DOMPDF_ENABLE_HTML5PARSER", true);
-    define("DOMPDF_ENABLE_FONTSUBSETTING", true);
-    define("DOMPDF_UNICODE_ENABLED", true);
-    define("DOMPDF_ENABLE_CSS_FLOAT", true);
-    define("DOMPDF_ENABLE_REMOTE", true);
+    if (!defined('DOMPDF_ENABLE_AUTOLOAD'))
+      define('DOMPDF_ENABLE_AUTOLOAD', false);
+
+    if (!defined("DOMPDF_ENABLE_HTML5PARSER"))
+      define("DOMPDF_ENABLE_HTML5PARSER", true);
+
+    if (!defined("DOMPDF_ENABLE_FONTSUBSETTING"))
+      define("DOMPDF_ENABLE_FONTSUBSETTING", true);
+
+    if (!defined("DOMPDF_UNICODE_ENABLED"))
+      define("DOMPDF_UNICODE_ENABLED", true);
+
+    if (!defined("DOMPDF_ENABLE_CSS_FLOAT"))
+      define("DOMPDF_ENABLE_CSS_FLOAT", true);
+
+    if (!defined("DOMPDF_ENABLE_REMOTE"))
+      define("DOMPDF_ENABLE_REMOTE", true);
 
     // include DOMPDF's default configuration
     require_once sfConfig::get('sf_plugins_dir').DIRECTORY_SEPARATOR.'bdProjectBasePlugin/lib/vendor/dompdf/dompdf_config.inc.php';
